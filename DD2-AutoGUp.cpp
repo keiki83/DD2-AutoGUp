3a8ed26b9fc8be300b6bd0bdd52f3cd38a4fc5ac
keiki83 <zyelor@yahoo.com>
2016-07-06 15:05:15 -0700

    naming

--- DD2-AutoGUp.cpp
+++ DD2-AutoGUp.cpp
@@ -1,6 +1,6 @@
 // Developed by Keiki83
 
-#define version 1.02
+#define version 1.03
 #include <windows.h>
 #include <stdlib.h>
 #include <iostream>
@@ -16,23 +16,23 @@ int main() {
   HWND gameWindow = FindWindow(NULL, "Dungeon Defenders 2");
   if(gameWindow == NULL) {
     std::cout << "ERROR: Dungeon Defenders 2 is not running." << std::endl;
-    std::cout << "DD2 Auto G Up closing in 10 seconds..." << std::endl;
+    std::cout << "DD2-AutoGUp closing in 10 seconds..." << std::endl;
     Sleep(10000);
     return 0;
   }
 
   // Information
-  std::cout << "DD2 Auto G UP was written by Keiki83, version " <<
+  std::cout << "DD2-AutoGUp was written by Keiki83, version " <<
     version << std::endl;
   std::cout << "This program is automatic and will detect your interaction with DD2.\n" <<
     "You do not need to start and stop the program.\n" <<
     "Simply tab-out for DD2-AutoGUp to take over.\n" << std::endl;
 
   // Primary Loop
-  PrintChat(gameWindow, "Player has started DD2 - Auto G Up");
-  std::cout << "DD2 Auto G Up executing in 10 seconds..." << std::endl;
+  PrintChat(gameWindow, "Player has started DD2-AutoGUp");
+  std::cout << "DD2-AutoGUp executing in 10 seconds..." << std::endl;
   Sleep(10000);
-  std::cout << "DD2 Auto G Up executing NOW!" << std::endl;
+  std::cout << "DD2-AutoGUp executing NOW!" << std::endl;
   while(true) {
     if(gameWindow != GetForegroundWindow())
       TransmitKeyStrokes(gameWindow, "EG");
