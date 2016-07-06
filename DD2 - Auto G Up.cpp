12e0f80ea3fc98f8a28603cd5946cf1afbf1b7fd
Michael Callan <mcallan@uci.edu>
2016-07-06 12:26:22 -0700

    Initial Push

---
+++ DD2 - Auto G Up.cpp
@@ -0,0 +1,61 @@
+// Developed by Keiki83
+
+#define version 1.01
+#include <windows.h>
+#include <stdlib.h>
+#include <iostream>
+
+void PrintChat(HWND, std::string);
+void TransmitKeyStrokes(HWND, std::string);
+
+int main() {
+  // Adjust Console size
+  system("mode 70, 10");
+
+  // Get DD2 window handle
+  HWND gameWindow = FindWindow(NULL, "Dungeon Defenders 2");
+  if(gameWindow == NULL) {
+    std::cout << "ERROR: Dungeon Defenders 2 is not running." << std::endl;
+    std::cout << "DD2 Auto G Up closing in 10 seconds..." << std::endl;
+    Sleep(10000);
+    return 0;
+  }
+
+  // Information
+  std::cout << "DD2 Auto G UP was written by Keiki83, version " <<
+    version << std::endl;
+  std::cout << "This program is automatic and will detect your interaction" <<
+  " with DD2.\nYou do not need to start and stop the program.\nSimply tab-out" <<
+  " for DD2 Auto G UP to take over.\n" << std::endl;
+
+  // Primary Loop
+  PrintChat(gameWindow, "Player has started DD2 - Auto G Up");
+  std::cout << "DD2 Auto G Up executing in 10 seconds..." << std::endl;
+  Sleep(10000);
+  std::cout << "DD2 Auto G Up executing NOW!" << std::endl;
+  while(true) {
+    if(gameWindow != GetForegroundWindow())
+      TransmitKeyStrokes(gameWindow, "EG");
+    Sleep(3000);
+  }
+
+  return 0;
+}
+
+// Trasmit key strokes to arg1 based on contents of arg2
+void TransmitKeyStrokes(HWND window, std::string value) {
+  for(int i = 0; i < value.length(); i++) {
+    SendMessage(window, WM_KEYDOWN, value[i], 0);
+    SendMessage(window, WM_KEYUP, value[i], 0);
+  }
+}
+
+// Trasmit message of arg2 to player chat screen (arg1)
+void PrintChat(HWND window, std::string message) {
+  SendMessage(window, WM_KEYDOWN, VK_RETURN, 0);
+  SendMessage(window, WM_KEYUP, VK_RETURN, 0);
+  for(int i = 0; i < message.length(); i++)
+    SendMessage(window, WM_CHAR, message[i], 0);
+  SendMessage(window, WM_KEYDOWN, VK_RETURN, 0);
+  SendMessage(window, WM_KEYUP, VK_RETURN, 0);
+}
